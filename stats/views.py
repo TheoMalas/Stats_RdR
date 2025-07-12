c4e25d836e5912e2df689cb7a33b022771a1929d
BusterAnarchy <busteranarchy68@gmail.com>
2025-07-12 20:00:37 +0200

    Single page app to server side rendering (#24)
    * move to server side rendering
    
    * fix button
    
    ---------
    
    Co-authored-by: Buster <buster.anarchy68@gmail.com>

--- stats/views.py
+++ stats/views.py
@@ -16,104 +16,143 @@ default_Delta = 15
 # FrontEnd
 
 def molecules_view(request):
+
+  date_debut, date_fin = get_dates(request)
+  familles_list = get_familles(request)
+
+  all_molecules_data = runScript("all/pie_chart_all_molecules", [date_debut, date_fin] + familles_list)    
+  area_all_molecules_data = runScript("all/stacked_area_prop_all_molecules", [date_debut, date_fin] + familles_list)
+
   return render(request, 'pages/all_molecules.html', { 
-      'fetch_url' : 'chart-data',
-      'area_stack' : 'chart-data',
-      'area3_stack' : 'chart-stacked-area-prop-all-molecules'
+      'all_molecules_data': all_molecules_data,
+      'area_all_molecules_data' : area_all_molecules_data,
   })
 
 def supply_view(request):
-  return render(request, 'pages/all_molecules.html', { 
-      'fetch_url' : 'chart-data-supply',
-      'area_stack' : 'chart-data-supply'
+
+  date_debut, date_fin = get_dates(request)
+  familles_list = get_familles(request)
+  data = runScript("all/pie_chart_supply_all_molecules", [date_debut, date_fin] + familles_list)
+
+  return render(request, 'pages/supply.html', { 
+      'data' : data,
   })
 
 def cocaine_view(request):
-    return render(request, 'pages/all_molecules.html', { 
-    'fetch_url' : 'chart-data-cocaine-coupe',
-    'area_stack' : 'chart-data-cocaine-coupe'
+
+  date_debut, date_fin = get_dates(request)
+  data = runScript("cocaine/diagram_coupe_cocaine", [date_debut, date_fin])
+
+  return render(request, 'pages/coupe.html', { 
+    'data' : data,
   })
 
 def heroine_view(request):
-    return render(request, 'pages/all_molecules.html', { 
-    'fetch_url' : 'chart-data-heroine-coupe',
-    'area_stack' : 'chart-data-heroine-coupe'
-  })
 
-def stacked_area_prop_all_molecules_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-stacked-area-prop-all-molecules',
-  'area_stack' : 'chart-stacked-area-prop-all-molecules'
+  date_debut, date_fin = get_dates(request)
+  data = runScript("heroine/diagram_coupe_heroine", [date_debut, date_fin])
+  
+  return render(request, 'pages/coupe.html', { 
+    'data' : data,
   })
 
 def purity_cocaine_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-cocaine',
-  'area2_stack' : 'chart-purity-cocaine'
+
+  date_debut, date_fin = get_dates(request)  
+  data = runScript("cocaine/histo_purity_cocaine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+  
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "Cocaine",
   })
 
 def purity_mdma_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-mdma',
-  'area2_stack' : 'chart-purity-mdma'
+
+  date_debut, date_fin = get_dates(request)  
+  data = runScript("mdma/histo_purity_mdma", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+  
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "MDMA",
   })
 
 def purity_heroine_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-heroine',
-  'area2_stack' : 'chart-purity-heroine'
+
+  date_debut, date_fin = get_dates(request)
+  data = runScript("heroine/histo_purity_heroine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "Héroine",
   })
 
 def purity_3mmc_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-3mmc',
-  'area2_stack' : 'chart-purity-3mmc'
+
+  date_debut, date_fin = get_dates(request)
+  data = runScript("3mmc/histo_purity_3mmc", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "3MMC",
   })
 
 def purity_ketamine_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-ketamine',
-  'area2_stack' : 'chart-purity-ketamine'
+
+  date_debut, date_fin = get_dates(request)
+  data = runScript("ketamine/histo_purity_ketamine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+  
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "Kétamine",
   })
 
 def purity_speed_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-speed',
-  'area2_stack' : 'chart-purity-speed'
+
+  date_debut, date_fin = get_dates(request)
+  data = runScript("speed/histo_purity_speed", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+  
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "Speed",
   })
 
 def purity_cannabis_THC_resine_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-cannabis-THC-resine',
-  'area2_stack' : 'chart-purity-cannabis-THC-resine'
+
+  date_debut, date_fin = get_dates(request)  
+  data = runScript("cannabis/histo_purity_cannabis_THC_resine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+  
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "Cannabis Resine",
   })
 
 def purity_cannabis_THC_herbe_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-purity-cannabis-THC-herbe',
-  'area2_stack': 'chart-purity-cannabis-THC-herbe'
+
+  date_debut, date_fin = get_dates(request)
+  data = runScript("cannabis/histo_purity_cannabis_THC_herbe", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
+    'molecule_name': "Cannabis Herbe",
   })
 
 def histo_comprime_mdma_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-histo-comprime-mdma',
-  'area2_stack' : 'chart-histo-comprime-mdma'
-  })
 
-def evol_purity_cocaine_view(request):
-  return render(request, 'pages/all_molecules.html', {
-  'fetch_url' : 'chart-evol-purity-cocaine'
+  date_debut, date_fin = get_dates(request)
+  data = runScript("mdma/histo_comprime_mdma", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
+    
+  return render(request, 'pages/purity.html', { 
+    'data' : data,
   })
 
 # BackEnd
 
 def runScript(scriptID, args):
-
   outputPath = 'output/' + scriptID + '.json'
 
   cachedData = basicCache(outputPath)
   if cachedData != None:
-      return JsonResponse(cachedData)
+      return cachedData
 
   cmd=["Rscript","scriptR/" + scriptID + ".R"] + args
   subprocess.run(cmd)
@@ -122,7 +161,7 @@ def runScript(scriptID, args):
   with open(json_file_path, 'r') as f:
       data = json.load(f)
 
-  return JsonResponse(data)
+  return data
 
 def basicCache(path):
 
@@ -161,76 +200,3 @@ def get_familles(request):
       familles_list = []
 
   return familles_list
-
-def chart_data(request):
- 
-  date_debut, date_fin = get_dates(request)
-  familles_list = get_familles(request)
-
-  return runScript("all/pie_chart_all_molecules", [date_debut, date_fin] + familles_list)
-  
-def chart_data_supply(request):
-  date_debut, date_fin = get_dates(request)
-  familles_list = get_familles(request)
-
-  return runScript("all/pie_chart_supply_all_molecules", [date_debut, date_fin] + familles_list)
-
-def chart_stacked_area_prop_all_molecules(request):
-  date_debut, date_fin = get_dates(request)
-
-  # Paramètre types (liste séparée par virgules dans l’URL)
-  familles_str = request.GET.get("familles")
-  if isinstance(familles_str, str):
-      familles_list = familles_str.split(",")
-  else:
-      familles_list = []
-      
-  return runScript("all/stacked_area_prop_all_molecules", [date_debut, date_fin] + familles_list)
-
-def chart_data_cocaine_coupe(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("cocaine/diagram_coupe_cocaine", [date_debut, date_fin])
-
-def chart_data_heroine_coupe(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("heroine/diagram_coupe_heroine", [date_debut, date_fin])
-
-def chart_purity_cocaine(request):
-  date_debut, date_fin = get_dates(request)  
-  return runScript("cocaine/histo_purity_cocaine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_purity_heroine(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("heroine/histo_purity_heroine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_purity_mdma(request):
-  date_debut, date_fin = get_dates(request)  
-  return runScript("mdma/histo_purity_mdma", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_purity_3mmc(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("3mmc/histo_purity_3mmc", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_purity_ketamine(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("ketamine/histo_purity_ketamine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_purity_speed(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("speed/histo_purity_speed", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_purity_cannabis_THC_resine(request):
-  date_debut, date_fin = get_dates(request)  
-  return runScript("cannabis/histo_purity_cannabis_THC_resine", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_purity_cannabis_THC_herbe(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("cannabis/histo_purity_cannabis_THC_herbe", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
-
-def chart_evol_purity_cocaine(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("cocaine/Evol_purity_cocaine", [date_debut, date_fin])
-
-def chart_histo_comprime_mdma(request):
-  date_debut, date_fin = get_dates(request)
-  return runScript("mdma/histo_comprime_mdma", [date_debut, date_fin, str(request.GET.get("range", default_Delta))])
